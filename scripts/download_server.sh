c351f6ecca0db7e7029fe94977a369b731e56817
Janis <janis.meister@outlook.com>
2025-07-04 01:17:42 +0200

    Remove redundant chmod command

--- scripts/download_server.sh
+++ scripts/download_server.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-EXEC=server.out
-
 echo "[Info] Downloading latest server version..."
-wget https://github.com/surrealtm/Glassminers/releases/latest/download/GMServer.out -O "$EXEC"
+wget https://github.com/surrealtm/Glassminers/releases/latest/download/GMServer.out -O "server.out"
 
 if [ $? -ne 0 ]; then
   echo "[Error] Download failed! Aborting."
   exit 1
 fi
 
-chmod +x "$EXEC"
 echo "[Info] Download completed successfully."
