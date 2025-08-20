0e3ba7ab8a08cb0b758f15223fbfbb18959ebfee
surrealtm <matheke.victor@gmail.com>
2025-08-20 21:28:02 +0200

    Added a second container that runs nightly server builds.

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,13 @@
 FROM debian:12.11-slim
 
+# The server version can be either 'nightly' or 'stable' and will differ in the
+# executable that is downloaded from github. These arguments are used at build-time
+ARG SERVER_VERSION
+ARG SERVER_PORT
+
+# This one is used at run-time
+ENV SERVER_VERSION ${SERVER_VERSION}
+
 RUN apt-get update && apt-get install -y wget
 
 ENV MAX_LOGS="20"
@@ -14,8 +22,8 @@ COPY ./scripts/download_server.sh ./
 COPY ./scripts/server_script.sh ./
 
 RUN chmod 777 ./*.sh
-RUN ./download_server.sh
+RUN ./download_server.sh $SERVER_VERSION
 
-CMD ["./server_script.sh"]
+CMD ./server_script.sh ${SERVER_VERSION}
 
-EXPOSE 9876
+EXPOSE $SERVER_PORT
--- scripts/download_server.sh
+++ scripts/download_server.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
-echo "[Info] Downloading latest server version..."
-wget https://github.com/surrealtm/Glassminers/releases/latest/download/GMServer.out -O "server.out"
+echo "[Info] Downloading server executable..."
+
+LOCAL_EXECUTABLE_NAME="server_$1.out"
+
+if [ "$1" = "stable" ]; then
+  wget https://github.com/surrealtm/Glassminers/releases/latest/download/GMServer.out -O $LOCAL_EXECUTABLE_NAME
+elif [ "$1" = "nightly" ]; then
+  wget https://github.com/surrealtm/Glassminers/raw/refs/heads/main/run_tree/server.out -O $LOCAL_EXECUTABLE_NAME
+else
+  echo "[Error] Please provide either 'nightly' or 'stable' as an argument to this script."
+  exit 1
+fi
 
 if [ $? -ne 0 ]; then
   echo "[Error] Download failed! Aborting."
   exit 1
 fi
 
-chmod 777 ./server.out
+chmod 777 $LOCAL_EXECUTABLE_NAME
 
 echo "[Info] Download completed successfully."
